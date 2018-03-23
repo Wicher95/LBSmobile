e46f46c1bf93269d93f75edffc6525a53568f8eb
Wicher95 <wicher.1995@gmail.com>
2018-03-23 16:29:30 +0100

    Poprawki do logowania fb

--- app/src/main/java/javy/od/swiry/lbsmobile/MainActivity.java
+++ app/src/main/java/javy/od/swiry/lbsmobile/MainActivity.java
@@ -1,12 +1,17 @@
 package javy.od.swiry.lbsmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -17,36 +22,48 @@ public class MainActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private static final String EMAIL = "email";
     private LoginButton loginButton;
+    private TextView isLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
+        isLogin = findViewById(R.id.isLogin);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void facebookLogin(View v)
     {
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton = findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList(EMAIL));
-        // If you are using in a fragment, call loginButton.setFragment(this);
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>()
+                {
+                    @Override
+                    public void onSuccess(LoginResult loginResult)
+                    {
+                        isLogin.setText("Zalogowano");
+                        Toast.makeText(MainActivity.this,"Login sucess",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancel()
+                    {
+                        isLogin.setText("Logowanie anulowano");
+                        Toast.makeText(MainActivity.this,"Login canceled",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception)
+                    {
+                        isLogin.setText("Logowanie nie udane");
+                        Toast.makeText(MainActivity.this,"Login failed",Toast.LENGTH_LONG).show();
+                    }
+                });
     }
-    /*
-    // Callback registration
-    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            // App code
-        }
-
-        @Override
-        public void onCancel() {
-            // App code
-        }
-
-        @Override
-        public void onError(FacebookException exception) {
-            // App code
-        }
-    });*/
 }
