787a5f4365c00a227029352655511627ce47d6dc
Wicher95 <wicher.1995@gmail.com>
2018-03-23 13:05:42 +0100

    Wstepne dodanie logowania przez facebooka

--- app/src/main/java/javy/od/swiry/lbsmobile/MainActivity.java
+++ app/src/main/java/javy/od/swiry/lbsmobile/MainActivity.java
@@ -2,12 +2,51 @@ package javy.od.swiry.lbsmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
     }
+
+    public void facebookLogin(View v)
+    {
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+    }
+    /*
+    // Callback registration
+    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            // App code
+        }
+
+        @Override
+        public void onCancel() {
+            // App code
+        }
+
+        @Override
+        public void onError(FacebookException exception) {
+            // App code
+        }
+    });*/
 }
