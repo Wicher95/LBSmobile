d2feb1a6bce3e936ea6bd84c05237042964cd915
Slawkoo94 <Slawkoo94@gmail.com>
2018-04-16 21:04:56 +0200

    Poprawki intent

--- app/src/main/java/javy/od/swiry/lbsmobile/StartActivity.java
+++ app/src/main/java/javy/od/swiry/lbsmobile/StartActivity.java
@@ -1,7 +1,9 @@
 package javy.od.swiry.lbsmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void gmailChange(View view) {
+        Intent myIntent = new Intent(this, RegisterActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void facebookChange(View view) {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
