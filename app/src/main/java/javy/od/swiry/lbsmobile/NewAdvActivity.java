418167f06b1b2d0149721c5cd376bcf5536bf239
Wicher <wicher.1995@gmail.com>
2018-05-21 20:20:28 +0200

    x

--- app/src/main/java/javy/od/swiry/lbsmobile/NewAdvActivity.java
+++ app/src/main/java/javy/od/swiry/lbsmobile/NewAdvActivity.java
@@ -1,13 +1,53 @@
 package javy.od.swiry.lbsmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NewAdvActivity extends AppCompatActivity {
+
+    public static final int PICK_IMAGE = 1;
+
+    private ImageView mGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_adv);
+
+        mGallery = findViewById(R.id.gallery);
+        galleryOnClick();
+    }
+
+    public void galleryOnClick(){
+        mGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickImage();
+            }
+        });
+    }
+
+    public void pickImage() {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+            //mGallery.setImageResource();
+        }
     }
 }
