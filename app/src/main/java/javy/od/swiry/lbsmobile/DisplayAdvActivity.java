73de2ecd8223d25eaecd0dc734240aa9ce9a77ec
Wicher <wicher.1995@gmail.com>
2018-05-25 13:10:23 +0200

    Ikonki menu, dodanie activity wyswietlajace opis ogloszenia

--- app/src/main/java/javy/od/swiry/lbsmobile/DisplayAdvActivity.java
+++ app/src/main/java/javy/od/swiry/lbsmobile/DisplayAdvActivity.java
@@ -1,33 +1,23 @@
 package javy.od.swiry.lbsmobile;
 
-import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.design.widget.*;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -35,41 +25,50 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javy.od.swiry.lbsmobile.models.Advert;
 
-public class MainMenuActivity extends AppCompatActivity {
-
+public class DisplayAdvActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
-    private ListView mAdvertList;
-    private ArrayList<Advert> listAdverts = new ArrayList<>();
-    private ArrayList<String> listIDs = new ArrayList<>();
-    private File tmpFile;
-    private ProgressDialog progressDialog;
+    private Context mContext;
+    private String ID;
     private boolean gotResult;
+    private ProgressDialog progressDialog;
     private Timer timer;
+    private Advert mAdvert;
+    private ImageView mImage;
+    private TextView mPrice;
+    private TextView mTitle;
+    private TextView mDate;
+    private MultiAutoCompleteTextView mDescription;
+    private TextView mLocalization;
+    private TextView mPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_display_adv);
 
         //Sprawdzenie czy użytkownik jest zalogowany
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
-            startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
+            startActivity(new Intent(mContext, MainActivity.class));
             Toast.makeText(this,"Aby kontynuować musisz się zalogować",Toast.LENGTH_SHORT).show();
         }
+
+        mContext = this;
+        progressDialog = new ProgressDialog(this);
+        mImage = findViewById(R.id.image);
+        mPrice = findViewById(R.id.price);
+        mTitle = findViewById(R.id.title);
+        mDate = findViewById(R.id.date);
+        mDescription = findViewById(R.id.description);
+        mLocalization = findViewById(R.id.localization);
+        mPhone = findViewById(R.id.phone);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -77,8 +76,6 @@ public class MainMenuActivity extends AppCompatActivity {
         Objects.requireNonNull(actionbar).setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         mDrawerLayout = findViewById(R.id.menu);
-        mAdvertList = findViewById(R.id.adverts);
-        progressDialog = new ProgressDialog(this);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
@@ -86,32 +83,32 @@ public class MainMenuActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                         mDrawerLayout.closeDrawers();
-
                         String itemTitle = String.valueOf(menuItem.getTitle());
                         if(itemTitle.equals("Dodaj ogłoszenie"))
                         {
-                            startActivity(new Intent(MainMenuActivity.this,NewAdvActivity.class));
+                            startActivity(new Intent(mContext,NewAdvActivity.class));
                         }
                         else if(itemTitle.equals("Ogłoszenia"))
                         {
-                            //startActivity(new Intent(MainActivity.this,MapsActivity.class));
+                            startActivity(new Intent(mContext,MainMenuActivity.class));
                         }
                         else if(itemTitle.equals("Moje ogłoszenia"))
                         {
-                            startActivity(new Intent(MainMenuActivity.this,UserAdvActivity.class));
+                            startActivity(new Intent(mContext,UserAdvActivity.class));
                         }
                         else if(itemTitle.equals("Wiadomości"))
                         {
                             //startActivity(new Intent(MainActivity.this,AddShopActivity.class));
                         } else if(itemTitle.equals("Wyloguj się")) {
                             FirebaseAuth.getInstance().signOut();
-                            startActivity(new Intent(MainMenuActivity.this, StartActivity.class));
+                            startActivity(new Intent(mContext, StartActivity.class));
                         }
                         return true;
                     }
                 });
-        generateAdv();
-        listHandler();
+
+        ID = getIntent().getStringExtra("ID");
+        downloadAdv();
     }
 
     //Otwieranie bocznego menu przyciskiem z toolbara
@@ -132,29 +129,23 @@ public class MainMenuActivity extends AppCompatActivity {
         //Sprawdzenie czy użytkownik jest zalogowany
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
-            startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
+            startActivity(new Intent(mContext, MainActivity.class));
             Toast.makeText(this,"Aby kontynuować musisz się zalogować",Toast.LENGTH_SHORT).show();
         }
-        generateAdv();
+        downloadAdv();
     }
 
-
-    public void generateAdv() {
-        progressDialog.setMessage("Wczytywanie ogłoszeń");
+    public void downloadAdv(){
+        progressDialog.setMessage("Wczytywanie ogłoszenia");
         progressDialog.show();
         DatabaseReference adverts = FirebaseDatabase.getInstance().getReference("adverts");
-        adverts.addValueEventListener(new ValueEventListener() {
+        adverts.child(ID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 gotResult = true;
-                listAdverts.clear();
-                listIDs.clear();
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    Advert advert = ds.getValue(Advert.class);
-                    if(advert != null) {
-                        listAdverts.add(0,advert);
-                        listIDs.add(0,ds.getKey());
-                    }
+                Advert advert = dataSnapshot.getValue(Advert.class);
+                if(advert != null) {
+                    mAdvert = advert;
                 }
                 displayAdv();
             }
@@ -171,7 +162,7 @@ public class MainMenuActivity extends AppCompatActivity {
                 timer.cancel();
                 if (!gotResult) { //  Timeout
                     FirebaseAuth.getInstance().signOut();
-                    Intent intent = new Intent(MainMenuActivity.this, StartActivity.class);
+                    Intent intent = new Intent(mContext, StartActivity.class);
                     intent.putExtra("fail","true");
                     startActivity(intent);
                 }
@@ -181,64 +172,14 @@ public class MainMenuActivity extends AppCompatActivity {
         timer.schedule(timerTask, 10000L);
     }
     public void displayAdv(){
-        if(listAdverts.size() > 0) {
-            CustomAdapter customAdapter = new CustomAdapter();
-            mAdvertList.setAdapter(customAdapter);
-        }
+        String price = mAdvert.getPrice() + " zł";
+        mPrice.setText(price);
+        mTitle.setText(mAdvert.getTitle());
+        mDate.setText(mAdvert.getDate());
+        mDescription.setText(mAdvert.getDescription());
+        mLocalization.setText(mAdvert.getLocalization());
+        mPhone.setText(mAdvert.getPhone());
+        Glide.with(mContext).load(mAdvert.getUrl()).into(mImage);
         progressDialog.dismiss();
-    }
-
-    private void listHandler() {
-        mAdvertList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int i, long l) {
-                TextView textView = v.findViewById(R.id.advID);
-                String advID = (String)textView.getText();
-                Intent intent = new Intent(getApplicationContext(), DisplayAdvActivity.class);
-                intent.putExtra("ID", advID);
-                startActivity(intent);
-            }
-        });
-    }
-
-
-
-    public class CustomAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return listAdverts.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @SuppressLint("ViewHolder")
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.adv_adapter, null);
-            TextView advID = convertView.findViewById(R.id.advID);
-            ImageView mainImage = convertView.findViewById(R.id.image);
-            TextView mainText = convertView.findViewById(R.id.name);
-            TextView city = convertView.findViewById(R.id.city);
-            TextView date = convertView.findViewById(R.id.date);
-            TextView price = convertView.findViewById(R.id.price);
-
-            advID.setText(listIDs.get(position));
-            Glide.with(MainMenuActivity.this).load(listAdverts.get(position).getUrl()).into(mainImage);
-            mainText.setText(listAdverts.get(position).getTitle());
-            city.setText(listAdverts.get(position).getLocalization());
-            date.setText(listAdverts.get(position).getDate());
-            String sPrice = listAdverts.get(position).getPrice() + " zł";
-            price.setText(sPrice);
-            return convertView;
-        }
     }
 }
